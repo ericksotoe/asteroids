1bffd690b77c41d846d9f7eea7d57d97097198fc
ericksotoe <ericksotoe@gmail.com>
2025-10-22 16:16:30 +0900

    set the FPS to 60 to save system resources

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # create a game loop 
     while True:
+        # for mac use, stops game when x bubble is clicked
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # create a blank black screen
         screen.fill(color=(0,0,0))
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
