3eae57a3e344933c6e5bace5927943814eab4d0c
ericksotoe <ericksotoe@gmail.com>
2025-10-23 23:06:53 +0900

    added groups and grouped up the players

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -6,6 +7,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # create groups for drawable and updatable obj
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # create player sprite
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     # create a game loop 
     while True:
@@ -15,8 +24,16 @@ def main():
                 return
         
         # create a blank black screen
+        updatable.update(dt)
         screen.fill(color=(0,0,0))
+
+        # draw each item in the group individually
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
+        
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
